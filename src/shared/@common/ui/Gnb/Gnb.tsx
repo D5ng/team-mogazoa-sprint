6e872cc9d0fcb96466fa77d433c833e638692cec
wooluck <155030236+parkwoohyeok@users.noreply.github.com>
2024-09-03 01:32:02 +0900

    ✨ feat: signIn여부에 따른 메뉴 분기처리(임시)

--- src/shared/@common/ui/Gnb/Gnb.tsx
+++ src/shared/@common/ui/Gnb/Gnb.tsx
@@ -3,6 +3,7 @@ import logo from '@/src/app/assets/images/logo.png'
 import logoMobile from '@/src/app/assets/images/logo-mobile.png'
 import Link from 'next/link'
 import GnbInput from './Gnb-Input'
+import GnbMenu from './Gnb-Menu'
 export default function Gnb() {
   return (
     <div className="flex items-center bg-black-20 px-120 py-40 justify-between">
@@ -12,12 +13,7 @@ export default function Gnb() {
       <div className="flex items-center gap-30">
         <GnbInput />
         <div className="flex text-white text-14 item-center gap-30">
-          <Link className="hover:text-blue" href={'/signIn'}>
-            로그인
-          </Link>
-          <Link className="hover:text-blue" href={'/signUp'}>
-            회원가입
-          </Link>
+          <GnbMenu />
         </div>
       </div>
     </div>
