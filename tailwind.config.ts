c68424827f61ea55bd550b42de96e1cf4297fba8
d5ng <dong5ffice@gmail.com>
2024-09-03 15:00:01 +0900

    🎨 [#11] Style: tailwind.config.ts 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
-const rem0_10 = { ...Array.from(Array(11)).map((_, i) => `${i / 10}rem`) }
-const rem0_100 = { ...Array.from(Array(101)).map((_, i) => `${i / 10}rem`) }
-const rem0_1300 = { ...Array.from(Array(1301)).map((_, i) => `${i / 10}rem`) }
-
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    borderRadius: {
-      s: '10px',
-      m: '15px',
-      l: '30px',
-      full: '100px',
-    },
-    gradientColor: {
-      'custom-gradient': 'linear-gradient(to right, #5097FA, #5363FF)',
-    },
     extend: {
-      borderWidth: rem0_10,
-      borderRadius: rem0_100,
-      fontSize: rem0_100,
-      lineHeight: rem0_100,
-      minWidth: rem0_1300,
-      minHeight: rem0_1300,
-      spacing: rem0_1300,
       colors: {
         blue: {
           DEFAULT: '#5097FA',
-        },
-        gray: {
-          10: '#6E6E82',
-          20: '#9FA6B2',
-          30: '#F1F1F5',
         },
         indigo: {
           DEFAULT: '#5363FF',
         },
         black: {
-          10: '#17171C',
-          20: '#21212A',
-          30: '#2E2E3A',
+          10: '#F1F1F5',
+          20: '#9FA6B2',
+          30: '#6E6E82',
+          40: '#2E2E3A',
+          50: '#21212A',
+          60: '#252530',
+          70: '#353542',
+          80: '#17171C',
         },
         red: '#FF0000',
         green: {
@@ -52,10 +32,7 @@ module.exports = {
         pink: {
           DEFAULT: '#FF2F9F',
         },
-        white: '#F1F1F5',
-        'beige-f9': '#F9F7F7',
         yellow: '#FFC83C',
-        disabled: '#353542',
       },
       screens: {
         tablet: { max: '1199px' },
