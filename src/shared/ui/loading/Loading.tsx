bef969b179c85fc2f685c254126d4366173adcb0
d5ng <dong5ffice@gmail.com>
2024-09-04 02:38:36 +0900

    ✨ [#19] Feat: 로딩 컴포넌트 추가

---
+++ src/shared/ui/loading/Loading.tsx
@@ -0,0 +1,18 @@
+export default function Loading() {
+  return (
+    <div className="absolute w-8 h-8 left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4">
+      <svg className="loading-circular" viewBox="25 25 50 50">
+        <circle
+          className="loading-path"
+          cx="50"
+          cy="50"
+          r="20"
+          fill="none"
+          stroke="#5363FF"
+          strokeWidth="4"
+          strokeMiterlimit="10"
+        />
+      </svg>
+    </div>
+  )
+}
