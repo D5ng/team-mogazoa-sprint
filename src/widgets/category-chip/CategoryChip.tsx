9338951ff52b0e7fb679aafbd1ed9bbe202a9ee3
d5ng <dong5ffice@gmail.com>
2024-09-04 15:10:48 +0900

    ♻️  [#25] Refactor: CategoryChip 컴포넌트 리팩토링

--- src/widgets/category-chip/CategoryChip.tsx
+++ src/widgets/category-chip/CategoryChip.tsx
@@ -8,10 +8,10 @@ interface CategoryChipProps {
 }
 
 export default function CategoryChip({ name }: CategoryChipProps) {
-  const category = CATEGORY_CHIPS.find((category) => category.name === name)
+  const category = CATEGORY_CHIPS.find((category) => category === name)
   if (!category)
     throw new Error('카테고리가 일치하지 않아요. 다시 확인해주세요.')
 
-  const className = `px-2.5 py-1 ${CATEGORY_COLOR_VARIANTS[category.name]}`
+  const className = `px-2.5 py-1 rounded-lg ${CATEGORY_COLOR_VARIANTS[category]}`
   return <span className={className}>{name}</span>
 }
