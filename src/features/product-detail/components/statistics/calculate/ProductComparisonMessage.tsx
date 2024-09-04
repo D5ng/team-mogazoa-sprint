6bb225d75b5992abde1109c1820a32df19b412c0
d5ng <dong5ffice@gmail.com>
2024-09-05 03:40:14 +0900

    ✨ [#30] Feat: ProductComparisonMessage 컴포넌트 추가

---
+++ src/features/product-detail/components/statistics/calculate/ProductComparisonMessage.tsx
@@ -0,0 +1,30 @@
+import { CountMessage, StarRatingMessage } from './helper'
+import { ProductComparisonMessageProps } from './ProductComparisonMessage.type'
+
+export default function ProductComparisonMessage({
+  title,
+  score,
+  scoreMetric,
+}: ProductComparisonMessageProps) {
+  const diff = +(score - scoreMetric).toFixed(2)
+  const isHigher = diff > 0 ? true : false
+  const isSame = score === scoreMetric
+
+  return (
+    <p className="text-center text-black-30 text-sm">
+      {title === '별점 평균' ? (
+        <StarRatingMessage
+          count={Math.abs(diff)}
+          isHigher={isHigher}
+          isSame={isSame}
+        />
+      ) : (
+        <CountMessage
+          count={Math.abs(diff)}
+          isHigher={isHigher}
+          isSame={isSame}
+        />
+      )}
+    </p>
+  )
+}
