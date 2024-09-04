c785ffac51858460768a8965a96ead96f3b6c5da
d5ng <dong5ffice@gmail.com>
2024-09-04 13:08:58 +0900

    ✨ [#23] Feat: 최신순 정렬 스토리북 추가

--- src/shared/ui/dropdown/Dropdown.stories.tsx
+++ src/shared/ui/dropdown/Dropdown.stories.tsx
@@ -5,11 +5,18 @@ import {
   DropdownMenu,
   DropdownMenuItem,
 } from './Dropdown'
+import type { DropdownVariantType } from './Dropdown.type'
 
-function DropdownStoryComponent({ menuItems }: { menuItems: string[] }) {
+interface DropdownProps {
+  menuItems: string[]
+  variant: DropdownVariantType
+  title: string
+}
+
+function DropdownStoryComponent({ menuItems, variant, title }: DropdownProps) {
   return (
-    <Dropdown>
-      <DropdownTrigger>카테고리 선택</DropdownTrigger>
+    <Dropdown variant={variant}>
+      <DropdownTrigger>{title}</DropdownTrigger>
       <DropdownMenu>
         {menuItems.map((menu) => (
           <DropdownMenuItem>{menu}</DropdownMenuItem>
@@ -19,16 +26,27 @@ function DropdownStoryComponent({ menuItems }: { menuItems: string[] }) {
   )
 }
 
-type Story = StoryObj<typeof DropdownStoryComponent>
+type DropdownStory = StoryObj<typeof DropdownStoryComponent>
 
 const meta: Meta<typeof DropdownStoryComponent> = {
   component: (args) => <DropdownStoryComponent {...args} />,
 }
 
-export const DropdownComponent: Story = {
+export const DropdownComponent: DropdownStory = {
   name: 'Dropdown',
   args: {
+    title: '카테고리 선택',
+    variant: 'border',
     menuItems: ['React.js', 'JavaScript', 'TypeScript'],
+  },
+}
+
+export const DropdownSortable: DropdownStory = {
+  name: '최신순 드롭다운',
+  args: {
+    title: '최신순',
+    variant: 'none',
+    menuItems: ['최신순', '별점 높은순', '별점 낮은순', '좋아요순'],
   },
 }
 
