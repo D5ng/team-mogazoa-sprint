9fdc9f0778806abdba723c1a8568c0ef1ca0081e
d5ng <dong5ffice@gmail.com>
2024-09-05 03:41:10 +0900

    ✨ [#30] Feat: Statistics 모듈 다시 내보내기

---
+++ src/features/product-detail/components/statistics/index.ts
@@ -0,0 +1,3 @@
+export { default as StatisticsList } from './list/StatisticsList'
+export { default as StatisticsListItem } from './item/StatisticsListItem'
+export { default as ProductComparisonMessage } from './calculate/ProductComparisonMessage'
