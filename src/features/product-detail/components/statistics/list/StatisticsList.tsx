57fab709b063d841f50916ab593f49d11f854e01
d5ng <dong5ffice@gmail.com>
2024-09-05 04:28:26 +0900

    ♻️ [#30] Refactor: StatisticsList 리팩토링

--- src/features/product-detail/components/statistics/list/StatisticsList.tsx
+++ src/features/product-detail/components/statistics/list/StatisticsList.tsx
@@ -1,23 +1,21 @@
 import { StatisticsListItem } from '@features/product-detail/components'
-import { STATISTICS_CARD, MOCK } from '@features/product-detail/constants'
-import { ProductDetailLayout } from '@features/product-detail/layout'
+import { STATISTICS_CARD } from '@features/product-detail/constants'
+import type { ProductDetailResponse } from '@features/product-detail/types'
 
-export default function StatisticsList() {
+export default function StatisticsList(statisticsData: ProductDetailResponse) {
   return (
-    <ProductDetailLayout title="상품 통계">
-      <ul className="flex gap-x-5 mobile:flex-col mobile:gap-x-0 mobile:gap-y-[15px] mobile:relative">
-        {STATISTICS_CARD.map((card) => {
-          const { name, ...data } = card
-          return (
-            <StatisticsListItem
-              key={card.name}
-              {...data}
-              score={MOCK[name]}
-              scoreMetric={MOCK.categoryMetric[name]}
-            />
-          )
-        })}
-      </ul>
-    </ProductDetailLayout>
+    <ul className="flex gap-x-5 mobile:flex-col mobile:gap-x-0 mobile:gap-y-[15px] mobile:relative">
+      {STATISTICS_CARD.map((card) => {
+        const { name, ...data } = card
+        return (
+          <StatisticsListItem
+            key={card.name}
+            {...data}
+            score={statisticsData[name]}
+            scoreMetric={statisticsData.categoryMetric[name]}
+          />
+        )
+      })}
+    </ul>
   )
 }
