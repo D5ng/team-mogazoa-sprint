4984a7cdc9a82a7d97c721b0ff8fcdb68ca7a53a
wooluck <155030236+parkwoohyeok@users.noreply.github.com>
2024-09-05 18:15:24 +0900

    [#32]✨ feat: category 컴포넌트 제작 및 반응형 완료

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,11 @@
 import Gnb from '@/src/widgets/Gnb/Gnb'
+import SideMenu from '@/src/widgets/side-menu/SideMenu'
 
 export default function main() {
   return (
     <>
       <Gnb />
+      <SideMenu />
     </>
   )
 }
