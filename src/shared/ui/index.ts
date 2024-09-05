a8b14902d5a9401d008e9d3cfef2b49a2c1538ae
JaeBeen95 <nin2348@gmail.com>
2024-09-05 21:37:10 +0900

    ✨ Feat: 경로 줄이기를 위한 index.ts설정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,9 +1,21 @@
 export { default as Button } from './button/Button'
-export { default as Form } from './form/Form'
 export { default as Loading } from './loading/Loading'
+export { default as Form } from './form/Form'
 export { default as TextFieldInput } from './text-field-input/TextFieldInput'
 
-export { Dropdown } from './dropdown/Dropdown'
+export {
+  AutocompleteField,
+  AutocompleteInput,
+  AutocompleteDropdown,
+} from './autocomplete-field/AutocompleteField'
+
+export {
+  FormField,
+  FieldLabel,
+  FieldInput,
+  FieldErrorMessage,
+} from './form-field/FormField'
+
 export {
   DropdownTrigger,
   DropdownMenu,
