ae17ec55f1e6207770cb403be82cfd61642caf39
d5ng <dong5ffice@gmail.com>
2024-09-06 01:42:36 +0900

    ♻️ [#13] Refactor: Dropdown type 리팩토링

--- src/shared/ui/dropdown/Dropdown.type.ts
+++ src/shared/ui/dropdown/Dropdown.type.ts
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react'
 
-export type DropdownVariantProps = 'border' | 'none'
+export type DropdownVariantType = 'border' | 'none'
 
 export interface DropdownProps {
   children: ReactNode
-  variant?: DropdownVariantProps
+  variant?: DropdownVariantType
 }
 
 export type DropdownContextType = {
@@ -14,9 +14,9 @@ export type DropdownContextType = {
   onCloseToggle: () => void
   selectedItem: string
   onSelect: (select: any) => void
-  variant: DropdownVariantProps
+  variant: DropdownVariantType
 }
 
-export type DropdownMenuItemType = {
+export interface DropdownMenuItemProps {
   children: ReactNode
 }
