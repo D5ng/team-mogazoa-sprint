c2e531bf564ec54dbe5a90b0e8c77df0e1891d69
wooluck <155030236+parkwoohyeok@users.noreply.github.com>
2024-09-06 12:54:01 +0900

    ♻️ refactor : ul li 적용

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,22 +1,16 @@
-
 import Gnb from '@/src/widgets/Gnb/Gnb'
 import SideMenu from '@/src/widgets/side-menu/SideMenu'
-
 
 import Ranking from '@/src/widgets/ranking/Ranking'
 
 const test = () => {
   return (
     <>
-
       <Gnb />
       <SideMenu />
-
       <Ranking />
-
     </>
   )
 }
 
 export default test
-
