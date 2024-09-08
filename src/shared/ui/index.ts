577a3f19a5feee99cdd528ff5e65fba2b3946e57
JaeBeen95 <nin2348@gmail.com>
2024-09-08 09:37:14 +0900

    ♻️ Refactor: export 호출 방식 변경

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -5,21 +5,6 @@ export { default as ImageInput } from './image-input/ImageInput'
 export { default as TextFieldInput } from './text-field-input/TextFieldInput'
 export { default as TextBoxInput } from './text-box-input/TextBoxInput'
 
-export {
-  AutocompleteField,
-  AutocompleteInput,
-  AutocompleteDropdown,
-} from './autocomplete-field/AutocompleteField'
-
-export {
-  FormField,
-  FieldLabel,
-  FieldInput,
-  FieldErrorMessage,
-} from './form-field/FormField'
-
-export {
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownMenuItem,
-} from './dropdown/Dropdown'
+export * from './autocomplete-field/AutocompleteField'
+export * from './form-field/FormField'
+export * from './dropdown/Dropdown'
