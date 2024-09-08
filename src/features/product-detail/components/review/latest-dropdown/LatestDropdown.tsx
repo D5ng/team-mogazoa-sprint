250d86a38b5907fdbfcda3730eef676ecde05795
d5ng <dong5ffice@gmail.com>
2024-09-08 10:32:50 +0900

    ♻️ [#40] Refactor: Latest Dropdown 리팩토링

--- src/features/product-detail/components/review/latest-dropdown/LatestDropdown.tsx
+++ src/features/product-detail/components/review/latest-dropdown/LatestDropdown.tsx
@@ -9,7 +9,7 @@ import { LATEST_DROPDOWN_ITEMS } from '@features/product-detail/constants'
 export default function LatestDropdown() {
   return (
     <Dropdown variant="none">
-      <DropdownTrigger className="text-white">최신순</DropdownTrigger>
+      <DropdownTrigger>최신순</DropdownTrigger>
       <DropdownMenu>
         {LATEST_DROPDOWN_ITEMS.map((title) => (
           <DropdownMenuItem key={title}>{title}</DropdownMenuItem>
