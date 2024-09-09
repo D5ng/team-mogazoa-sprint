e949eada3aabdec7e6ee7e690c1d6ddee2cd8249
JaeBeen95 <nin2348@gmail.com>
2024-09-09 17:51:45 +0900

    🔥 Remove: 불필요한 타입 제거

--- src/shared/ui/input/text-box-input/TextBoxInput.tsx
+++ src/shared/ui/input/text-box-input/TextBoxInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import { forwardRef } from 'react'
 import { TextBoxInputProps } from './TextBoxInput.type'
 
 const TextBoxInput = forwardRef<HTMLTextAreaElement, TextBoxInputProps>(
