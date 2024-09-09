a1cac7a319db45dd6a51a62c2a6429259b57d49a
d5ng <dong5ffice@gmail.com>
2024-09-09 14:59:23 +0900

    ✨ [#57] Feat: 공통 모달 컴포넌트 추가

---
+++ src/shared/ui/modal/Modal.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import { createContext, PropsWithChildren, useContext } from 'react'
+import { createPortal } from 'react-dom'
+import type { ModalContextType, ModalProps } from './Modal.type'
+import { close } from '@app/icons'
+
+const ModalContext = createContext<ModalContextType>({
+  onCloseModal: () => {},
+})
+
+const useModalContext = () => {
+  const modalContext = useContext(ModalContext)
+  if (!modalContext) throw new Error('Modal Context에서 사용해주세요.')
+  return modalContext
+}
+
+export function Modal(props: ModalProps) {
+  const element = document.getElementById('modal')!
+  return createPortal(
+    <ModalContext.Provider value={{ onCloseModal: props.onCloseModal }}>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[120] w-[620px] p-[40px] bg-black-80 text-white rounded-2xl">
+        <ModalBackdrop />
+        <ModalExit />
+        {props.children}
+      </div>
+    </ModalContext.Provider>,
+    element,
+  )
+}
+
+export function ModalBackdrop() {
+  const { onCloseModal } = useModalContext()
+  const element = document.getElementById('backdrop')!
+  return createPortal(
+    <div
+      className="fixed w-screen h-screen left-0 top-0 bg-[#000000b3] z-[110]"
+      onClick={onCloseModal}
+    ></div>,
+    element,
+  )
+}
+
+export function ModalExit() {
+  const { onCloseModal } = useModalContext()
+  return (
+    <button onClick={onCloseModal} className="absolute right-[20px] top-[20px]">
+      <Image src={close} alt="모달 창 닫기" width="40" height="40" />
+    </button>
+  )
+}
+
+export function ModalTitle({ children }: PropsWithChildren) {
+  return <h2 className="text-2xl font-semibold">{children}</h2>
+}
