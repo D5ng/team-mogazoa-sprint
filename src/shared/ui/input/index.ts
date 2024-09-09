48b67d61b924ade95fa029bf77b2794d1037a369
JaeBeen95 <nin2348@gmail.com>
2024-09-09 20:54:16 +0900

    🚚 Rename: 오타 수정

--- src/shared/ui/input/index.ts
+++ src/shared/ui/input/index.ts
@@ -1,3 +1,3 @@
 export { default as ImageInput } from './image-input/ImageInput'
-export { default as TextareaInput } from './textarea-input/TextAreaInput'
+export { default as TextareaInput } from './textarea-input/TextareaInput'
 export { default as TextFieldInput } from './text-field-input/TextFieldInput'
