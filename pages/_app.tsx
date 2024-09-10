030349df7c16e4b08da58295eb9cca4e370e920a
JaeBeen95 <nin2348@gmail.com>
2024-09-10 17:33:04 +0900

    ♻️ Refactor: 불필요 코드 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
 import '@/src/app/styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
-
   return (
     <>
       <Head>
@@ -16,9 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
       </Head>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <Component {...pageProps} />
     </>
   )
 }
