dfe3b20a071caa27da956296e2fc2307bf62f85e
d5ng <dong5ffice@gmail.com>
2024-09-10 23:16:05 +0900

    ♻️ [#75] Refactor: 불필요한 코드 제거

--- pages/dong/index.tsx
+++ pages/dong/index.tsx
@@ -1,5 +1,3 @@
-import ProductAddModal from '@features/product-add-modal/components/modal/ProductAddModal'
-import { useToggle } from '@/src/shared/hooks'
 import ProductAddButton from '@/src/widgets/product-add-button/ProductAddButton'
 
 export default function index() {
