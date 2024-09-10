45d8882c06ee474ed4e45a0730de86d1b6b732ec
d5ng <dong5ffice@gmail.com>
2024-09-10 23:14:26 +0900

    ✨ [#75] Feat: ProductAddButton 컴포넌트 추가

--- pages/dong/index.tsx
+++ pages/dong/index.tsx
@@ -1,13 +1,7 @@
 import ProductAddModal from '@features/product-add-modal/components/modal/ProductAddModal'
 import { useToggle } from '@/src/shared/hooks'
+import ProductAddButton from '@/src/widgets/product-add-button/ProductAddButton'
 
 export default function index() {
-  const { isToggle, onOpenToggle, onCloseToggle } = useToggle()
-
-  return (
-    <>
-      <button onClick={onOpenToggle}>Open toggle</button>
-      {isToggle && <ProductAddModal onCloseToggle={onCloseToggle} />}
-    </>
-  )
+  return <ProductAddButton />
 }
---
+++ src/widgets/product-add-button/ProductAddButton.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import { add } from '@app/icons'
+import { useToggle } from '@shared/hooks'
+import { ProductAddModal } from '@features/product-add-modal/components'
+
+export default function ProductAddButton() {
+  const { isToggle, onOpenToggle, onCloseToggle } = useToggle()
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={onOpenToggle}
+        className="fixed w-[60px] h-[60px] rounded-full gradient flex items-center justify-center right-[80px] bottom-[80px] mobile:right-[50px] mobile:bottom-[50px]"
+      >
+        <Image src={add} alt="제품 추가하기" width={40} height={40} />
+      </button>
+      {isToggle && <ProductAddModal onCloseToggle={onCloseToggle} />}
+    </>
+  )
+}
