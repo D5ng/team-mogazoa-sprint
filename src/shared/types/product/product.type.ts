6200661c4d9a2ec4adef09220ee8aa75fd5e7149
d5ng <dong5ffice@gmail.com>
2024-09-11 12:13:03 +0900

    ✨ [#78]: Product type 추가

---
+++ src/shared/types/product/product.type.ts
@@ -0,0 +1,35 @@
+export interface ProductResponse {
+  nextCursor: number
+  list: ProductListItem[]
+}
+
+export interface ProductListItem {
+  updatedAt: string
+  createdAt: string
+  writerId: number
+  categoryId: number
+  favoriteCount: number
+  reviewCount: number
+  rating: number
+  image: string
+  name: string
+  id: number
+}
+
+export interface FetchProducts {
+  keyword?: string
+  category?: number
+  order?: 'recent' | 'rating' | 'reviewCount'
+  cursor?: number
+}
+
+export type SendProductType = {
+  categoryId: number
+  image: File | null
+  description: string
+  name: string
+}
+
+export type ProductId = {
+  productId: number
+}
