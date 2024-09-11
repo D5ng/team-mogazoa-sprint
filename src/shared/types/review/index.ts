ca80aa04bf59466a9ffd94b6eba9da18ee863201
d5ng <dong5ffice@gmail.com>
2024-09-12 01:41:17 +0900

    ♻️ [#87] Refactor: Review API, Type 리팩토링

--- src/shared/types/review/index.ts
+++ src/shared/types/review/index.ts
@@ -4,6 +4,16 @@ export interface ReviewId {
 
 export interface CreateReview {
   productId: number
+  images: {
+    file: File
+    index: number
+  }[]
+  content: string
+  rating: number
+}
+
+export interface CreateReviewPayload {
+  productId: number
   images: string[]
   content: string
   rating: number
