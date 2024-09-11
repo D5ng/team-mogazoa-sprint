fed041d874b3b80665fbf604543892c6647ab399
d5ng <dong5ffice@gmail.com>
2024-09-11 12:12:55 +0900

    ✨ [#78]: Product API 추가

---
+++ src/shared/api/product.api.ts
@@ -0,0 +1,57 @@
+import axiosInstance from '@app/config/axios-instance'
+import {
+  FetchProducts,
+  FetchProductsReviews,
+  ProductDetailResponse,
+  ProductId,
+  ProductResponse,
+  SendProductType,
+} from '../types'
+
+export async function fetchProducts({
+  keyword,
+  category,
+  order,
+  cursor,
+}: FetchProducts) {
+  return (await axiosInstance.get<ProductResponse>(`/products`)).data
+}
+
+export async function fetchProductDetail({ productId }: ProductId) {
+  return (
+    await axiosInstance.get<ProductDetailResponse>(`/products/${productId}`)
+  ).data
+}
+
+export async function createProduct(data: SendProductType) {
+  return (await axiosInstance.post(`/products`, data)).data
+}
+
+export async function updateProduct({
+  productId,
+  ...data
+}: SendProductType & ProductId) {
+  return (
+    await axiosInstance.patch<SendProductType>(`/products/${productId}`, data)
+  ).data
+}
+
+export async function deleteProduct({ productId }: ProductId) {
+  return (await axiosInstance.delete(`/products/${productId}`)).data
+}
+
+export async function fetchProductsReviews({
+  productId,
+  order,
+  cursor,
+}: FetchProductsReviews) {
+  return (await axiosInstance.get(`/products/${productId}/reviews`)).data
+}
+
+export async function favoriteProduct({ productId }: ProductId) {
+  return (await axiosInstance.post(`/products/${productId}/favorite`)).data
+}
+
+export async function deleteFavoriteProduct({ productId }: ProductId) {
+  return (await axiosInstance.delete(`/products/${productId}/favorite`)).data
+}
