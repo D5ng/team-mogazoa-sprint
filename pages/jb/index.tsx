af94625e46282522979bd1141cac4c76f8301b20
JaeBeen95 <nin2348@gmail.com>
2024-09-11 12:30:30 +0900

    ⚗️ experiment: 로그아웃 테스트 성공

--- pages/jb/index.tsx
+++ pages/jb/index.tsx
@@ -1,7 +1,12 @@
 import { useAuth } from '@widgets/auth/hooks'
+import { Button } from '@shared/ui'
 
 export default function index() {
   const { logout } = useAuth()
 
-  return <button onClick={logout}>로그아웃</button>
+  return (
+    <Button variant="primary" onClick={logout}>
+      로그아웃
+    </Button>
+  )
 }
