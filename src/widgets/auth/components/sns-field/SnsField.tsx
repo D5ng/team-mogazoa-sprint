d4938eddae6ab9a14546d82aab50556f16e4b015
JaeBeen95 <nin2348@gmail.com>
2024-09-12 01:33:44 +0900

    ✨ Feat: Sns field에 함수 등록

--- src/widgets/auth/components/sns-field/SnsField.tsx
+++ src/widgets/auth/components/sns-field/SnsField.tsx
@@ -3,7 +3,7 @@ import { useOAuth } from '@widgets/auth/hooks'
 import { google, kakao } from '@shared/icons'
 
 export default function SnsField() {
-  const { googleSignIn } = useOAuth()
+  const { googleSignIn, kakaoSignIn } = useOAuth()
 
   return (
     <div className="flex flex-col gap-5 text-black-30">
@@ -12,13 +12,12 @@ export default function SnsField() {
         <button
           onClick={googleSignIn}
           className="p-3.5 border border-black-30 rounded-full"
-          aria-label="구글 로그인"
         >
           <Image src={google} alt="구글 로그인" width={24} height={24} />
         </button>
         <button
+          onClick={kakaoSignIn}
           className="p-3.5 border border-black-30 rounded-full"
-          aria-label="카카오 로그인"
         >
           <Image src={kakao} alt="카카오 로그인" width={24} height={24} />
         </button>
