2ae2a1db86a99fc6649b630c908995f06fabc5d3
d5ng <dong5ffice@gmail.com>
2024-09-13 01:07:46 +0900

    ♻️ [#93] Refactor: Statistics 폴더 구조 변경

--- src/widgets/product-detail/components/statistics/list-item/StatisticsListItem.tsx
+++ src/widgets/product-detail/components/statistics/list-item/StatisticsListItem.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image'
+import { useFetchProductDetail } from '@shared/hooks'
 import ProductComparisonMessage from '../calculate/ProductComparisonMessage'
 
 interface StatisticsListItemProps {
+  productId: number
   title: string
-  score: number
-  scoreMetric: number
-  image: string
+  icon: string
+  name: 'rating' | 'favoriteCount' | 'reviewCount'
 }
 
 export default function StatisticsListItem({
+  productId,
   title,
-  score,
-  scoreMetric,
-  image,
+  icon,
+  name,
 }: StatisticsListItemProps) {
+  const detailQuery = useFetchProductDetail(productId)
+  const data = detailQuery.data!
+
   return (
     <li className="w-[300px] h-[190px] flex flex-col justify-center items-center gap-y-5 bg-black-60 border border-black-70 rounded-xl tablet:w-full tablet:h-auto tablet:px-11 tablet:py-[30px] tablet:gap-x-[15px] tablet:gap-y-[15px] mobile:p-5 mobile:justify-start mobile:items-start">
       <div className="flex flex-col gap-y-5 items-center tablet:gap-y-[15px] mobile:flex-row mobile:gap-x-[10px]">
@@ -22,21 +26,21 @@ export default function StatisticsListItem({
         </h3>
         <div className="flex items-center gap-x-[5px]">
           <Image
-            src={image}
+            src={icon}
             alt=""
             width={24}
             height={24}
             className="tablet:w-5 tablet:h-5"
           />
           <span className="text-2xl font-light text-black-20 tablet:text-xl mobile:text-base">
-            {score}
+            {data[name]}
           </span>
         </div>
       </div>
       <ProductComparisonMessage
         title={title}
-        score={score}
-        scoreMetric={scoreMetric}
+        score={data[name]}
+        scoreMetric={data.categoryMetric[name]}
       />
     </li>
   )
--- src/widgets/product-detail/components/statistics/list/StatisticsList.tsx
+++ src/widgets/product-detail/components/statistics/list/StatisticsList.tsx
@@ -1,21 +1,27 @@
-import type { ProductDetailResponse } from '@shared/types'
 import { StatisticsListItem } from '@widgets/product-detail/components'
+import { ProductDetailLayout } from '@widgets/product-detail/layout'
 import { STATISTICS_CARD } from '@widgets/product-detail/constants'
 
-export default function StatisticsList(statisticsData: ProductDetailResponse) {
+interface StatisticsListProps {
+  productId: number
+}
+
+export default function StatisticsList({ productId }: StatisticsListProps) {
   return (
-    <ul className="flex gap-x-5 mobile:flex-col mobile:gap-x-0 mobile:gap-y-[15px] mobile:relative">
-      {STATISTICS_CARD.map((card) => {
-        const { name, ...data } = card
-        return (
-          <StatisticsListItem
-            key={card.name}
-            {...data}
-            score={statisticsData[name]}
-            scoreMetric={statisticsData.categoryMetric[name]}
-          />
-        )
-      })}
-    </ul>
+    <ProductDetailLayout title="상품 통계">
+      <ul className="flex gap-x-5 mobile:flex-col mobile:gap-x-0 mobile:gap-y-[15px] mobile:relative">
+        {STATISTICS_CARD.map((card) => {
+          const { name, ...data } = card
+          return (
+            <StatisticsListItem
+              key={card.name}
+              name={name}
+              productId={productId}
+              {...data}
+            />
+          )
+        })}
+      </ul>
+    </ProductDetailLayout>
   )
 }
