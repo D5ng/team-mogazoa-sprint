549a7b608505837fe09671f6a906a3bd448f7f03
d5ng <dong5ffice@gmail.com>
2024-09-13 01:10:19 +0900

    🔥 [#93] Remove: Mock Data 제거

--- src/widgets/product-detail/constants/Statistics.constants.ts
+++ src/widgets/product-detail/constants/Statistics.constants.ts
@@ -4,41 +4,16 @@ export const STATISTICS_CARD = [
   {
     name: 'rating',
     title: '별점 평균',
-    image: star,
+    icon: star,
   },
   {
     name: 'favoriteCount',
     title: '찜',
-    image: heart,
+    icon: heart,
   },
   {
     name: 'reviewCount',
     title: '리뷰',
-    image: review,
+    icon: review,
   },
 ] as const
-
-export const STATISTICS_MOCK = {
-  id: 1,
-  name: '에어팟 프로',
-  description: '노이즈 캔슬링이 잘 되는 이어폰',
-  image:
-    'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MWP22?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1591634795000',
-  rating: 4.5,
-  reviewCount: 100,
-  favoriteCount: 99,
-  categoryId: 1,
-  createdAt: '2024-08-09T04:07:45.676Z',
-  updatedAt: '2024-08-09T04:07:45.676Z',
-  writerId: 1,
-  isFavorite: false,
-  category: {
-    id: 1,
-    name: '전자기기',
-  },
-  categoryMetric: {
-    rating: 3.8,
-    favoriteCount: 120,
-    reviewCount: 80,
-  },
-} as const
