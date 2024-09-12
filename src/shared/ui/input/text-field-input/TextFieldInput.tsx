e610d4caa926d899cdf9435e2bbe55c184e23e83
JaeBeen95 <nin2348@gmail.com>
2024-09-13 02:20:32 +0900

    ✨ Feat: input 에러메시지 추가

--- src/shared/ui/input/text-field-input/TextFieldInput.tsx
+++ src/shared/ui/input/text-field-input/TextFieldInput.tsx
@@ -2,10 +2,17 @@ import { forwardRef } from 'react'
 import type { TextFieldInputProps } from '@shared/ui/input/text-field-input/TextFieldInput.type'
 
 const TextFieldInput = forwardRef<HTMLInputElement, TextFieldInputProps>(
-  ({ placeholder, setValue, name, ...props }, ref) => {
+  ({ placeholder, setValue, name, errors, onBlur, ...props }, ref) => {
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setValue(name, e.target.value)
     }
+
+    const error = errors[name]
+
+    const focusClassName = error
+      ? ''
+      : 'focus:ring-1 focus:ring-indigo focus:border-indigo'
+    const borderClassName = error ? 'border-red' : 'border-black-70'
 
     return (
       <div className={`relative w-full h-full ${props.className || ''}`}>
@@ -13,10 +20,12 @@ const TextFieldInput = forwardRef<HTMLInputElement, TextFieldInputProps>(
           id={name}
           name={name}
           onChange={handleInputChange}
+          onBlur={onBlur}
           placeholder={placeholder}
-          className="input-base"
+          className={`input-base ${focusClassName} ${borderClassName}`}
           ref={ref}
         />
+        <p className="text-red text-xs">{error && error.message}</p>
       </div>
     )
   },
--- src/shared/ui/input/textarea-input/TextareaInput.tsx
+++ src/shared/ui/input/textarea-input/TextareaInput.tsx
@@ -2,8 +2,15 @@ import { forwardRef } from 'react'
 import { TextareaInputProps } from './TextareaInput.type'
 
 const TextareaInput = forwardRef<HTMLTextAreaElement, TextareaInputProps>(
-  ({ placeholder, name, value, maxLength = 500, ...props }, ref) => {
+  ({ placeholder, name, value, maxLength = 500, errors, ...props }, ref) => {
     const textLength = value ? value.length : 0
+
+    const error = errors[name]
+
+    const focusClassName = error
+      ? ''
+      : 'focus:ring-1 focus:ring-indigo focus:border-indigo'
+    const borderClassName = error ? 'border-red' : 'border-black-70'
 
     return (
       <div
@@ -14,13 +21,14 @@ const TextareaInput = forwardRef<HTMLTextAreaElement, TextareaInputProps>(
           name={name}
           placeholder={placeholder}
           maxLength={maxLength}
-          className={`input-base resize-none overflow-y-auto ${props.className || ''}`}
+          className={`input-base resize-none overflow-y-auto ${focusClassName} ${borderClassName} ${props.className || ''}`}
           ref={ref}
           {...props}
         />
         <span className="absolute right-5 bottom-5 text-sm text-black-30">
           {textLength}/{maxLength}
         </span>
+        <p className="text-red text-xs">{error && error.message}</p>
       </div>
     )
   },
