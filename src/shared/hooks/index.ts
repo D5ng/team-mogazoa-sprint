818e0baca6612bdb354283a15ef6c591b2cd9296
d5ng <dong5ffice@gmail.com>
2024-09-13 01:10:47 +0900

    ✨ [#93] Feat: useFetchProductDetail 훅 추가

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -3,3 +3,5 @@ export { default as useOutsideClick } from './useOutsideClick'
 export { default as useSelect } from './useSelect'
 export { default as useAutocomplete } from './useAutoComplete'
 export { default as useKeyboardSelect } from './useKeyboardSelect'
+
+export * from './query'
---
+++ src/shared/hooks/query/product-detail.query.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchProductDetail } from '@shared/api'
+import { ProductDetailResponse } from '@shared/types'
+
+export function useFetchProductDetail(
+  productId: number,
+  initialData?: ProductDetailResponse,
+) {
+  return useQuery({
+    queryKey: ['product-detail'],
+    queryFn: () => fetchProductDetail({ productId }),
+    initialData,
+  })
+}
