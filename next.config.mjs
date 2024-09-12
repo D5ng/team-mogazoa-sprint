3c3b1b46e8ba247ed6974701afeec1b34fa82f90
d5ng <dong5ffice@gmail.com>
2024-09-13 01:11:41 +0900

    🔧 [#93] Chore: 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['pbs.twimg.com'],
+    domains: [
+      'pbs.twimg.com',
+      'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+    ],
   },
 }
 
