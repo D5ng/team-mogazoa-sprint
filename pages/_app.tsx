cbae43aa0b1854d827fc565153c950e8ebf55c5f
d5ng <dong5ffice@gmail.com>
2024-09-13 01:01:08 +0900

    ✨ [#93] Feat: React Query 초기 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,24 @@
+import { useState } from 'react'
+import type { AppProps } from 'next/app'
 import '@/src/app/styles/globals.css'
 import Gnb from '@/src/widgets/product/gnb/Gnb'
-
-import type { AppProps } from 'next/app'
-import { useEffect, useState } from 'react'
+import {
+  QueryClient,
+  QueryClientProvider,
+  HydrationBoundary,
+} from '@tanstack/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [mounted, setMounted] = useState<boolean>(false)
+  const [queryClient] = useState(() => new QueryClient())
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  //위 로직은 로컬에서 하이드레이션 버그를 예방하기 위해서 넣었습니다! 빌드시 제거해야합니다.
   return (
-    mounted && (
-      <>
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
         <Gnb />
         <div className="mt-[100px] tablet:mt-[80px] mobile:mt-[70px]">
           <Component {...pageProps} />
         </div>
-      </>
-    )
+      </HydrationBoundary>
+    </QueryClientProvider>
   )
 }
