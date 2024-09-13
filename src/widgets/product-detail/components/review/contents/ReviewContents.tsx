6260d049972536764e5f06f101561cda0fd39196
d5ng <dong5ffice@gmail.com>
2024-09-13 11:35:47 +0900

    ✨ [#93] Feat: AuthStroe 수정 및 리뷰 수정 삭제 인증

--- src/widgets/product-detail/components/review/contents/ReviewContents.tsx
+++ src/widgets/product-detail/components/review/contents/ReviewContents.tsx
@@ -1,6 +1,7 @@
 import { ProductReviewItem } from '@shared/types'
 import ThumbsButton from './thumbs-button/ThumbsButton'
 import ReviewImages from './review-images/ReviewImages'
+import useAuthStore from '@/src/shared/store/authStore'
 
 export default function ReviewContents({
   content,
@@ -13,16 +14,20 @@ export default function ReviewContents({
   ProductReviewItem,
   'content' | 'updatedAt' | 'isLiked' | 'reviewImages' | 'userId' | 'likeCount'
 >) {
+  const user = useAuthStore().user
+
   return (
     <div className="relative w-[calc(100%-150px-80px)] flex flex-col gap-y-5 text-base tablet:text-xs mobile:w-full">
       <p>{content}</p>
       {!!reviewImages.length && <ReviewImages reviewImages={reviewImages} />}
       <div className="flex items-end gap-x-5 mobile:gap-x-3">
         <span className="text-black-30">{updatedAt}</span>
-        <div className="flex gap-x-2 text-black-20">
-          <button className="underline">수정</button>
-          <button className="underline">삭제</button>
-        </div>
+        {userId === user?.id && (
+          <div className="flex gap-x-2 text-black-20">
+            <button className="underline">수정</button>
+            <button className="underline">삭제</button>
+          </div>
+        )}
       </div>
     </div>
   )
