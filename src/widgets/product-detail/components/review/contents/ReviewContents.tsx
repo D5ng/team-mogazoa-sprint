16ad6795274d1f6931bbe0c0daa8b92634b63f7f
d5ng <dong5ffice@gmail.com>
2024-09-13 13:56:53 +0900

    🐛  [#93] Fix: 리뷰 날짜 수정

--- src/widgets/product-detail/components/review/contents/ReviewContents.tsx
+++ src/widgets/product-detail/components/review/contents/ReviewContents.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { ProductReviewItem } from '@shared/types'
 import ThumbsButton from './thumbs-button/ThumbsButton'
 import ReviewImages from './review-images/ReviewImages'
@@ -7,7 +8,6 @@ import ReviewUpdateButton from './update-button/ReviewUpdateButton'
 
 export default function ReviewContents(props: ProductReviewItem) {
   const user = useAuthStore().user
-
   return (
     <div className="relative w-[calc(100%-150px-80px)] flex flex-col gap-y-5 text-base tablet:text-xs mobile:w-full">
       <p>{props.content}</p>
@@ -15,7 +15,9 @@ export default function ReviewContents(props: ProductReviewItem) {
         <ReviewImages reviewImages={props.reviewImages} />
       )}
       <div className="flex items-end gap-x-5 mobile:gap-x-3">
-        <span className="text-black-30">{props.updatedAt}</span>
+        <span className="text-black-30">
+          {dayjs(props.updatedAt).format('YYYY-MM-DD')}
+        </span>
         {props.userId === user?.id && (
           <div className="flex gap-x-2 text-black-20">
             <ReviewUpdateButton {...props} />
