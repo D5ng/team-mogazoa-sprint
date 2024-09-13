40c43d3cd6af43b790630349cd6a63e65d1740df
d5ng <dong5ffice@gmail.com>
2024-09-13 14:05:37 +0900

    💄 [#93] Design: 리뷰 업데이트 모달 UI 변경

--- src/widgets/product-review-modal/components/modal/ReviewUpdateModal.tsx
+++ src/widgets/product-review-modal/components/modal/ReviewUpdateModal.tsx
@@ -15,12 +15,6 @@ export default function ReviewUpdateModal({
   return (
     <Modal onCloseModal={onCloseToggle}>
       <ModalTitle>리뷰 수정하기</ModalTitle>
-      <div className="flex mt-2.5 gap-x-2.5">
-        <h2 className="text-2xl font-semibold tablet:text-xl">
-          MacBook Air 13 M2 칩
-        </h2>
-        <CategoryChip name="전자기기" />
-      </div>
       <ReviewUpdateForm {...props} onCloseToggle={onCloseToggle} />
     </Modal>
   )
