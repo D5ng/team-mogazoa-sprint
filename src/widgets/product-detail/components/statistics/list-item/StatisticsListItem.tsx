7c944c0f8c5a3492fb021c876acbb08cf62a052a
d5ng <dong5ffice@gmail.com>
2024-09-13 10:59:50 +0900

    🐛 [#93] Fix: 통계 데이터 소수점 문제 해결

--- src/widgets/product-detail/components/statistics/list-item/StatisticsListItem.tsx
+++ src/widgets/product-detail/components/statistics/list-item/StatisticsListItem.tsx
@@ -17,6 +17,8 @@ export default function StatisticsListItem({
 }: StatisticsListItemProps) {
   const detailQuery = useFetchProductDetail(productId)
   const data = detailQuery.data!
+  const dataScore = data[name].toFixed(1).split('.')
+  const transformedScore = +dataScore[1] ? data[name].toFixed(1) : data[name]
 
   return (
     <li className="w-[300px] h-[190px] flex flex-col justify-center items-center gap-y-5 bg-black-60 border border-black-70 rounded-xl tablet:w-full tablet:h-auto tablet:px-11 tablet:py-[30px] tablet:gap-x-[15px] tablet:gap-y-[15px] mobile:p-5 mobile:justify-start mobile:items-start">
@@ -33,7 +35,7 @@ export default function StatisticsListItem({
             className="tablet:w-5 tablet:h-5"
           />
           <span className="text-2xl font-light text-black-20 tablet:text-xl mobile:text-base">
-            {data[name]}
+            {transformedScore}
           </span>
         </div>
       </div>
