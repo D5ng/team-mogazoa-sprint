64072059abb1ccf9e22caa0c2efc836fa1a70fe9
d5ng <dong5ffice@gmail.com>
2024-09-14 02:45:25 +0900

    ✨ [#93] Feat: 클립보드 기능 추가

--- src/widgets/product-detail/components/product-info/product-contents/ClipBoardButton.tsx
+++ src/widgets/product-detail/components/product-info/product-contents/ClipBoardButton.tsx
@@ -2,8 +2,20 @@ import Image from 'next/image'
 import { share } from '@shared/icons'
 
 export default function ClipBoardButton() {
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href)
+      alert(`클리보드가 복사되었어요`)
+    } catch (err) {
+      console.error('클리보드를 복사하는데 에러가 발생했어요')
+    }
+  }
+
   return (
-    <button className="flex justify-center items-center w-[28px] h-[28px] rounded-md bg-black-60 tablet:w-[24px] tablet:h-[24px]">
+    <button
+      className="flex justify-center items-center w-[28px] h-[28px] rounded-md bg-black-60 tablet:w-[24px] tablet:h-[24px]"
+      onClick={copyToClipboard}
+    >
       <Image
         src={share}
         alt="클립보드 복사하기"
