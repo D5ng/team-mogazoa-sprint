0950670ae2e84c89595651cc596818479d607e95
JaeBeen95 <nin2348@gmail.com>
2024-09-14 16:34:55 +0900

    💄 Style: 레이아웃 간격 조절

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -5,11 +5,9 @@ export default function Profile() {
   return (
     <main className="flex justify-center gap-[60px] w-full mt-[160px] tablet:flex-col tablet:px-28 mobile:mt-[130px] mobile:px-4">
       <ProfileCard />
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-[60px]">
         <ActivityLog />
-        <section className="col-start-2 tablet:col-start-1">
-          오른쪽 아래 요소
-        </section>
+        <section className="col-start-2 tablet:col-start-1">카드자리</section>
       </div>
     </main>
   )
