47715d7493b8f46afe2a226fa0f6c6155974d1b3
d5ng <dong5ffice@gmail.com>
2024-09-14 23:41:56 +0900

    ✨ [#93] Feat: 리뷰 옵션 전역 상태 관리

---
+++ src/shared/store/review.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+import {
+  LATEST_DROPDOWN_ITEMS,
+  ReviewSortOptions,
+} from '@widgets/product-detail/constants'
+
+interface ReviewStore {
+  option: ReviewSortOptions
+  onSelectedOption: (selectedOption: ReviewSortOptions) => void
+}
+
+export const useReviewOptionStore = create<ReviewStore>((set) => {
+  return {
+    option: LATEST_DROPDOWN_ITEMS[0].value,
+    onSelectedOption: (selectedOption: ReviewSortOptions) =>
+      set(() => ({ option: selectedOption })),
+  }
+})
+
+export default useReviewOptionStore
