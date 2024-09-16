2fd7371300f3a81ab5c22ec46e07fb3c2bd72651
d5ng <dong5ffice@gmail.com>
2024-09-16 23:05:35 +0900

    ♻️ [#104] Refactor: 리팩토링

--- src/widgets/auth/components/nickname-field/NicknameField.tsx
+++ src/widgets/auth/components/nickname-field/NicknameField.tsx
@@ -7,9 +7,11 @@ import {
 } from '@shared/ui/form-field/FormField'
 import { nicknameValidation } from '@widgets/auth/lib/form-validation'
 import type { NicknameFieldData } from '@shared/types'
-import type { NicknameFieldProps } from '@widgets/auth/components/nickname-field/NicknameField.type'
+import { useRouter } from 'next/router'
+import { GET_SOCIAL_API } from '@shared/constants'
 
 export default function NicknameField() {
+  const router = useRouter()
   const {
     register,
     handleSubmit,
@@ -23,7 +25,9 @@ export default function NicknameField() {
   })
 
   const onSubmit = async (data: NicknameFieldData) => {
-    const uri = `https://kauth.kakao.com/oauth/authorize?client_id=ca3528350b4d4fd6a868e561b970f195&redirect_uri=http://localhost:3000/api/kakao/signUp&response_type=code&state=${encodeURIComponent(data.nickname)}`
+    const socialType = router.pathname.includes('kakao') ? 'kakao' : 'google'
+    const uri = GET_SOCIAL_API(data.nickname)[socialType]
+
     window.location.href = uri
   }
 
