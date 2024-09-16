a0e36e6414daec9c62328930f0aabeb0e4d8684c
JaeBeen95 <nin2348@gmail.com>
2024-09-16 21:41:41 +0900

    ✨ Feat: 프로필 카드 섹션 메뉴 반응형 분기

--- src/widgets/profile/components/product-card-section/ProductCardSection.tsx
+++ src/widgets/profile/components/product-card-section/ProductCardSection.tsx
@@ -5,9 +5,19 @@ import {
   WishListedProducts,
 } from '@widgets/profile/components'
 import { PRODUCT_CATEGORIES } from '@widgets/profile/constants/ProductCategories.constant'
+import {
+  Dropdown,
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownTrigger,
+} from '@shared/ui'
+import useWindowResize from '@shared/hooks/useWindowResize'
 
 export default function ProductCardSection() {
-  const [activeCategory, setActiveCategory] = useState('리뷰 남긴 상품')
+  const [activeCategory, setActiveCategory] = useState(
+    PRODUCT_CATEGORIES[0].title,
+  )
+  const windowSize = useWindowResize()
 
   const componentMap = {
     ReviewedProducts,
@@ -21,21 +31,44 @@ export default function ProductCardSection() {
         ?.componentName || 'ReviewedProducts'
     ]
 
+  const isTablet = windowSize < 1024
+
   return (
     <section className="flex flex-col gap-4">
-      <div className="flex gap-4">
-        {PRODUCT_CATEGORIES.map(({ title }) => (
-          <button
-            key={title}
-            onClick={() => setActiveCategory(title)}
-            className={`text-xl tablet:text-lg ${
-              activeCategory === title ? 'text-white' : 'text-black-30'
-            }`}
-          >
-            {title}
-          </button>
-        ))}
-      </div>
+      {isTablet ? (
+        <Dropdown variant="none">
+          <DropdownTrigger className="whitespace-nowrap tablet:text-sm">
+            {activeCategory}
+          </DropdownTrigger>
+          <DropdownMenu>
+            {PRODUCT_CATEGORIES.map((category) => (
+              <DropdownMenuItem
+                key={category.title}
+                onClick={() => setActiveCategory(category.title)}
+              >
+                {category.title}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+      ) : (
+        <div className="flex gap-4">
+          {PRODUCT_CATEGORIES.map(({ title }) => {
+            const isActive = activeCategory === title
+            const textColor = isActive ? 'text-white' : 'text-black-30'
+
+            return (
+              <button
+                key={title}
+                onClick={() => setActiveCategory(title)}
+                className={`text-xl whitespace-nowrap ${textColor}`}
+              >
+                {title}
+              </button>
+            )
+          })}
+        </div>
+      )}
       <ActiveComponent />
     </section>
   )
