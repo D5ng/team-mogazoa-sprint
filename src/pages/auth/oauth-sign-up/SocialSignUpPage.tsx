a3e8c10a019882c2c02b9baab84ec1ed31d7a63e
d5ng <dong5ffice@gmail.com>
2024-09-16 23:48:16 +0900

    ♻️ [#104] Refactor: 파일 및 폴더 이름 변경

--- src/pages/auth/oauth-sign-up/SocialSignUpPage.tsx
+++ src/pages/auth/oauth-sign-up/SocialSignUpPage.tsx
@@ -1,6 +1,6 @@
 import NicknameField from '@widgets/auth/components/nickname-field/NicknameField'
 
-export default function SocialSignUpPage() {
+export default function OauthSignUpPage() {
   return (
     <main className="w-full min-h-screen flex justify-center items-center mt-24 tablet:mt-48 mobile:mt-20 bg-black-80">
       <NicknameField />
--- src/pages/auth/sign-in/SignInPage.tsx
+++ src/pages/auth/sign-in/SignInPage.tsx
@@ -1,11 +1,11 @@
 import { SignInField } from '@widgets/auth/components'
-import SnsField from '@widgets/auth/components/sns-field/SnsField'
+import { OauthField } from '@widgets/auth/components'
 
 export default function SingInPage() {
   return (
     <main className="flex flex-col gap-14 justify-center items-center mt-48 bg-black-80">
       <SignInField />
-      <SnsField />
+      <OauthField />
     </main>
   )
 }
