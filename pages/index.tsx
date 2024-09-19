f907564afa823921610cd93272e3f00caff1cc10
d5ng <dong5ffice@gmail.com>
2024-09-19 19:57:32 +0900

    ♻️ Refactor: Home 페이지 리팩토링

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,7 @@
-import {
-  ProductHot,
-  Ranking,
-  SideMenu,
-  ProductRating,
-} from '@widgets/product/product-home/components'
-import useWindowResize from '@shared/hooks/useWindowResize'
-import ProductAddButton from '@/src/widgets/product/product-home/components/product-add-button/ProductAddButton'
+import ProductPage from '@/src/pages/product/Product'
 
 const home = () => {
-  const WINDOW_SIZE = useWindowResize()
-
-  return (
-    <div className="flex justify-between tablet:px-[0px]  mt-[100px] tablet:mt-[80px] mobile:mt-[70px]  ">
-      <div className="tablet:w-full ml-[10vw] tablet:ml-[0] mobile:ml-[3vw] tablet:px-[5vw] flex gap-[1vw] tablet:gap-[6vw] ">
-        {WINDOW_SIZE > 767 && <SideMenu />}
-        <div className="flex flex-col ml-[1.9vw] gap-[5vw] tablet:flex tablet:flex-col">
-          <ProductHot />
-          <ProductRating />
-        </div>
-        <Ranking />
-      </div>
-      <ProductAddButton />
-    </div>
-  )
+  return <ProductPage />
 }
 
 export default home
