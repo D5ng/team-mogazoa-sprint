e4f17d45665abd4d348e7f555969a33a0c244c97
JaeBeen95 <nin2348@gmail.com>
2024-09-19 16:19:02 +0900

    ✨ Feat: 비로그인 유저 상품 생성 모달 막는 로직

--- src/widgets/product-add-button/ProductAddButton.tsx
+++ src/widgets/product-add-button/ProductAddButton.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image'
+import { toast } from 'react-toastify'
 import { add } from '@shared/icons'
 import { useToggle } from '@shared/hooks'
 import { ProductAddModal } from '@widgets/product-add-modal/components'
+import { useUserStore } from '@shared/store'
 
 export default function ProductAddButton() {
+  const { user } = useUserStore()
   const { isToggle, onOpenToggle, onCloseToggle } = useToggle()
+
+  const openModal = () =>
+    user ? onOpenToggle() : toast.error('로그인이 필요합니다')
 
   return (
     <>
       <button
         type="button"
-        onClick={onOpenToggle}
+        onClick={openModal}
         className="fixed w-[60px] h-[60px] rounded-full gradient flex items-center justify-center right-[80px] bottom-[80px] mobile:right-[50px] mobile:bottom-[50px]"
       >
         <Image src={add} alt="제품 추가하기" width={40} height={40} />
