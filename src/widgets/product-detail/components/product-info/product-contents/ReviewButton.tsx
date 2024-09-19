000cf0874a8c514552c3a65a80f27497d7ca2d9a
JaeBeen95 <nin2348@gmail.com>
2024-09-19 16:21:04 +0900

    ✨ Feat: 비로그인 유저 리뷰 작성 모달 로직 막기

--- src/widgets/product-detail/components/product-info/product-contents/ReviewButton.tsx
+++ src/widgets/product-detail/components/product-info/product-contents/ReviewButton.tsx
@@ -1,6 +1,8 @@
-import { useToggle } from '@shared/hooks'
-import { Button } from '@shared/ui'
+import { toast } from 'react-toastify'
 import { ReviewCreateModal } from '@widgets/product-review-modal/components'
+import { useToggle } from '@shared/hooks'
+import { useUserStore } from '@shared/store'
+import { Button } from '@shared/ui'
 
 interface ReviewButtonProps {
   productId: number
@@ -13,13 +15,17 @@ export default function ReviewButton({
   categoryName,
   productName,
 }: ReviewButtonProps) {
+  const { user } = useUserStore()
   const { isToggle, onCloseToggle, onOpenToggle } = useToggle()
+
+  const openModal = () =>
+    user ? onOpenToggle() : toast.error('로그인이 필요합니다')
   return (
     <>
       <Button
         variant="primary"
         className="w-[345px] h-[65px] tablet:w-[33.065vw] tablet:h-[55px] mobile:w-full"
-        onClick={onOpenToggle}
+        onClick={openModal}
       >
         리뷰 작성하기
       </Button>
