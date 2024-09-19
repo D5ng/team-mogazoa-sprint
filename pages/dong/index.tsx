67eb5d4f2d790d22c177e0d363ba275acd53e81c
d5ng <dong5ffice@gmail.com>
2024-09-19 18:17:30 +0900

    🔥 Remove: 불필요한 폴더 제거

--- pages/dong/index.tsx
+++
@@ -1,5 +0,0 @@
-import ProductAddButton from '@/src/widgets/product/product-home/components/product-add-button/ProductAddButton'
-
-export default function index() {
-  return <ProductAddButton />
-}
