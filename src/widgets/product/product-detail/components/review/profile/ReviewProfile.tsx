54bc14b69056e07138d89b853731e34da9943daf
d5ng <dong5ffice@gmail.com>
2024-09-20 13:30:41 +0900

    ✨ [#125] Feat: 리뷰 프로필 추가

--- src/widgets/product/product-detail/components/review/profile/ReviewProfile.tsx
+++ src/widgets/product/product-detail/components/review/profile/ReviewProfile.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image'
+import { defaultProfile } from '@shared/icons'
 import type { ProductReviewItem, ReviewUser } from '@shared/types'
-import ReviewProfileImage from '@public/test/review-profile.jpg'
 import ReviewRating from './rating/ReviewRating'
 
 export default function ReviewProfile({
   nickname,
   rating,
+  image,
 }: Pick<ReviewUser, 'nickname' | 'image'> & Pick<ProductReviewItem, 'rating'>) {
   return (
-    <div className="flex items-center gap-x-2.5 w-[140px]">
+    <div className="flex items-center gap-x-2.5 w-[150px]">
       <div className="relative w-[42px] h-[42px] rounded-full overflow-hidden tablet:w-[36px] tablet:h-[36px] tablet:text-sm ">
-        {/* <Image src={ReviewProfileImage} alt="" fill /> */}
+        <Image src={image || defaultProfile} alt="" fill />
       </div>
       <div className="flex flex-col gap-y-[5px]">
         <span className="text-base">{nickname}</span>
