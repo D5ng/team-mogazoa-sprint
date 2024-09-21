d6108bad4472ce4c4b0729d153cd2c2ae6995baa
JaeBeen95 <nin2348@gmail.com>
2024-09-21 23:45:15 +0900

    ✨ Feat: product-detail의 이미지에도 적용

--- src/widgets/product/product-detail/components/product-info/product-image/ProductImage.tsx
+++ src/widgets/product/product-detail/components/product-info/product-image/ProductImage.tsx
@@ -1,3 +1,4 @@
+import { useImageFallback } from '@shared/hooks/useImageFallback'
 import Image from 'next/image'
 
 interface ProductImageProps {
@@ -5,14 +6,17 @@ interface ProductImageProps {
 }
 
 export default function ProductImage({ image }: ProductImageProps) {
+  const { imageSrc, onError } = useImageFallback(image)
+
   return (
     <div className="relative w-[355px] tablet:w-[37.634vw] mobile:w-full mobile:h-[62.933vw]">
       <Image
-        src={image}
+        src={imageSrc}
         alt=""
         fill
         className="object-cover object-center"
         priority
+        onError={onError}
       />
     </div>
   )
