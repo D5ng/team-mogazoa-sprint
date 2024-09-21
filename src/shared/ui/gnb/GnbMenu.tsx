f7402fafe9453133d6acec5b9d14a0ec01a54cd3
wooluck <155030236+parkwoohyeok@users.noreply.github.com>
2024-09-21 14:41:34 +0900

    ♻️ refactor : category 선택에 따른 query처리

--- src/shared/ui/gnb/GnbMenu.tsx
+++ src/shared/ui/gnb/GnbMenu.tsx
@@ -4,11 +4,13 @@ import { useRouter } from 'next/router'
 
 export default function GnbMenu() {
   const { user: isSigned } = useAuthStore()
+
   const router = useRouter()
   const { id: productId } = router.query
 
   const MENU_DEFAULT_STYLE =
     'hover:text-blue mobile:text-black-20 rounded-xl  font-semibold mobile:px-[10px] mobile:py-[2px] mobile:text-[14px] mobile:hover:border-blue  mobile:border-black-20 mobile:border mobile:'
+
   return (
     <>
       {isSigned ? (
@@ -30,10 +32,10 @@ export default function GnbMenu() {
         <div
           className={`flex gap-[40px] mobile:gap-[20px] mobile:flex-col  mobile:text-center justify-center shrink-0`}
         >
-          <Link className={`${MENU_DEFAULT_STYLE}`} href="/sign-in">
+          <Link className={`${MENU_DEFAULT_STYLE}`} href="/auth/sign-in">
             로그인
           </Link>
-          <Link className={`${MENU_DEFAULT_STYLE}`} href="/sign-up">
+          <Link className={`${MENU_DEFAULT_STYLE}`} href="/auth/sign-up">
             회원가입
           </Link>
         </div>
--- src/widgets/product/product-home/components/category-menu/CategoryMenu.tsx
+++ src/widgets/product/product-home/components/category-menu/CategoryMenu.tsx
@@ -1,4 +1,3 @@
-import { forwardRef } from 'react'
 import { useProductStore } from '@shared/store/productStore'
 import useSearchProduct from '@shared/hooks/useSearchProduct'
 import { CATEGORY_CHIPS } from '@shared/ui'
@@ -13,20 +12,19 @@ const CategoryMenu = ({ menuVisible }: CategoryMenuProps) => {
   const { selectedCategoryKey } = useProductStore()
   const { handleCategory } = useSearchProduct()
   const router = useRouter()
-
-  if (router.pathname !== '/') return null
-
+  if (router.pathname !== '/' && router.pathname !== `/category/[id]`) return
   return (
     <article
-      className={`fixed top-105 mt-[20px] mobile:mt-[50px] tablet:w-full shrink-0 left-[9.5vw] tablet:left-[4vw] ${menuVisible ? '' : 'mobile:hidden'} mobile:w-2/5 mobile:left-0 mobile:bg-black-50 mobile:z-dropdown`}
+      className={`fixed top-105 mt-[20px] mobile:mt-[50px]  shrink-0 left-[9.5vw]  tablet:left-[4vw] ${menuVisible ? '' : 'mobile:hidden'} mobile:w-2/5 mobile:left-0 mobile:bg-black-50 mobile:z-dropdown`}
     >
       <p className="text-[16px] tablet:text-[14px] mobile:mt-[20px] ml-[20px] text-white mb-[10px]">
         카테고리
       </p>
-      <ul className="flex flex-col gap-[8px] mobile:mt-[20px]">
+      <ul className="flex flex-col gap-[8px] mobile:mt-[20px] w-[200px] mobile:w-[180px]">
         {CATEGORY_CHIPS.map((data) => (
           <CategoryItems
             key={data.id}
+            query={data.id}
             checked={selectedCategoryKey === data.id}
             handleClick={() => handleCategory(data.id, data.name)}
           >
