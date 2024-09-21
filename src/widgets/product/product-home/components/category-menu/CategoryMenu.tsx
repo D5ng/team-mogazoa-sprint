ab0dbf7adaecdb3dfa75ff071efd3a69a969c27f
d5ng <dong5ffice@gmail.com>
2024-09-21 23:51:57 +0900

    ✨ [#137] Feat: 카테고리 UI 자잘한 수정

--- src/widgets/product/product-home/components/category-menu/CategoryMenu.tsx
+++ src/widgets/product/product-home/components/category-menu/CategoryMenu.tsx
@@ -1,7 +1,6 @@
 import { useProductStore } from '@shared/store/productStore'
 import useSearchProduct from '@shared/hooks/useSearchProduct'
 import { CATEGORY_CHIPS } from '@shared/ui'
-import { useRouter } from 'next/router'
 import CategoryItems from './CategoryItems'
 
 interface CategoryMenuProps {
@@ -11,8 +10,7 @@ interface CategoryMenuProps {
 const CategoryMenu = ({ menuVisible }: CategoryMenuProps) => {
   const { selectedCategoryKey } = useProductStore()
   const { handleCategory } = useSearchProduct()
-  const router = useRouter()
-  if (router.pathname !== '/' && router.pathname !== `/category/[id]`) return
+
   return (
     <article
       className={`fixed top-105 mt-[20px] mobile:mt-[50px]  shrink-0 left-[9vw]  tablet:left-[40px] ${menuVisible ? '' : 'mobile:hidden'} mobile:w-2/5 mobile:left-0 mobile:bg-black-50 mobile:z-dropdown`}
