f3d3fda58704dd87b374ddc728818e6decab68b7
d5ng <dong5ffice@gmail.com>
2024-09-22 17:28:09 +0900

    ✨ [#144] Feat: SEO 누락된 페이지 추가

--- pages/auth/sign-in/index.tsx
+++ pages/auth/sign-in/index.tsx
@@ -1,5 +1,11 @@
 import { SignInPage } from '@/src/pages/'
+import { NextSeo } from 'next-seo'
 
 export default function SignIn() {
-  return <SignInPage />
+  return (
+    <>
+      <NextSeo title="모가조아 - 로그인" />
+      <SignInPage />
+    </>
+  )
 }
