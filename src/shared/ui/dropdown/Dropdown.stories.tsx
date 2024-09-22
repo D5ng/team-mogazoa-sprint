acf37e3ea48c9f3dfac76ee08b28da9deae61bc9
d5ng <dong5ffice@gmail.com>
2024-09-23 00:56:48 +0900

    ✨ [#155] Feat: 드롭다운 스토리북 추가

--- src/shared/ui/dropdown/Dropdown.stories.tsx
+++ src/shared/ui/dropdown/Dropdown.stories.tsx
@@ -9,8 +9,8 @@ import type { DropdownVariantType } from './Dropdown.type'
 
 interface DropdownProps {
   menuItems: string[]
-  variant: DropdownVariantType
   title: string
+  variant?: DropdownVariantType
 }
 
 function DropdownStoryComponent({
@@ -33,28 +33,67 @@ function DropdownStoryComponent({
 type Story = StoryObj<typeof DropdownStoryComponent>
 
 const meta: Meta<typeof DropdownStoryComponent> = {
-  component: DropdownStoryComponent,
-}
-
-export const DropdownComponent: Story = {
-  name: 'Dropdown',
+  title: 'UI/Dropdown',
   argTypes: {
-    title: {
-      control: 'text',
-      description: '타이틀 텍스트를 설정해주는 부분',
-    },
     variant: {
       control: 'select',
       options: ['border', 'none'],
+      description: '드롭다운 스타일 종류 설정입니다.',
+      table: {
+        defaultValue: {
+          summary: 'border',
+        },
+      },
+    },
+    title: {
+      control: 'text',
+      description: '드롭다운 기본 타이틀을 설정입니다.',
+      table: {
+        defaultValue: {
+          summary: '',
+        },
+      },
     },
     menuItems: {
       control: 'object',
+      description:
+        '드롭다운 아이템 설정입니다. 배열 형태로 전달해 주어야합니다.',
     },
   },
+  component: DropdownStoryComponent,
+}
+
+export const DropdownExample: Story = {
+  name: 'Dropdown Example',
   args: {
+    menuItems: ['카테고리1', '카테고리2', '카테고리3', '카테고리4'],
     title: '카테고리 선택',
     variant: 'border',
-    menuItems: ['React.js', 'JavaScript', 'TypeScript'],
+  },
+  render: () => (
+    <DropdownStoryComponent
+      menuItems={['카테고리1', '카테고리2', '카테고리3', '카테고리4']}
+      title="카테고리 선택"
+      variant="border"
+    />
+  ),
+}
+
+export const DropdownBorderType: Story = {
+  name: 'Dropdown Border Type',
+  args: {
+    menuItems: ['카테고리1', '카테고리2', '카테고리3', '카테고리4'],
+    title: '카테고리 선택',
+    variant: 'border',
+  },
+}
+
+export const DropdownBorderNoneType: Story = {
+  name: 'Dropdown Border None Type',
+  args: {
+    menuItems: ['카테고리1', '카테고리2', '카테고리3', '카테고리4'],
+    title: '카테고리1',
+    variant: 'none',
   },
 }
 
