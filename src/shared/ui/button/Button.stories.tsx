1ef3bc165526ce95163d4c5da278ec0c6d9e6abf
d5ng <dong5ffice@gmail.com>
2024-09-23 00:17:43 +0900

    📝  [#153] Docs: 카테고리 수정

--- src/shared/ui/button/Button.stories.tsx
+++ src/shared/ui/button/Button.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import Button from './Button'
 
 const meta: Meta<typeof Button> = {
+  title: 'UI/Button',
   component: Button,
   argTypes: {
     variant: {
