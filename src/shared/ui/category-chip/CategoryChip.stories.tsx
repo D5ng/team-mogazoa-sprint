46967320b1005b82eeb2df49a0d0158ad9c4b803
d5ng <dong5ffice@gmail.com>
2024-09-23 12:36:45 +0900

    ✨ [#157] Feat: 카테고리 칩 스토리북 추가

--- src/shared/ui/category-chip/CategoryChip.stories.tsx
+++ src/shared/ui/category-chip/CategoryChip.stories.tsx
@@ -1,24 +1,36 @@
 import { Meta, StoryObj } from '@storybook/react'
 import CategoryChip from './CategoryChip'
+import type { CategoryName } from './CategoryChip.type'
 import { CATEGORY_CHIPS } from './CategoryChip.constants'
 
 type Story = StoryObj<typeof CategoryChip>
 
 const meta: Meta<typeof CategoryChip> = {
+  title: 'UI/CategoryChip',
   component: CategoryChip,
 }
 
 export const CategoryChipStory: Story = {
-  name: 'widgets/CategoryChip',
+  name: 'Category Example',
   argTypes: {
     name: {
-      control: 'radio',
-      options: CATEGORY_CHIPS,
+      options: CATEGORY_CHIPS.map((chip) => chip.name),
     },
   },
   args: {
     name: CATEGORY_CHIPS[0].name,
   },
+}
+
+export const CategoryVariants: Story = {
+  name: 'Category Variants',
+  render: () => (
+    <div className="flex gap-x-2.5">
+      {CATEGORY_CHIPS.map((categoryChip) => (
+        <CategoryChip name={categoryChip.name} key={categoryChip.id} />
+      ))}
+    </div>
+  ),
 }
 
 export default meta
