2365ece3fc9d9ce65ffcf20bb4fe677883e418f8
d5ng <dong5ffice@gmail.com>
2024-09-26 23:41:48 +0900

    💄 [#186] Design: 프로필 sticky 변경

--- src/widgets/profile/components/profile-card/card/UserProfileCard.tsx
+++ src/widgets/profile/components/profile-card/card/UserProfileCard.tsx
@@ -24,7 +24,7 @@ export default function UserProfileCard({
   return (
     <section
       className={twMerge(
-        'flex flex-col justify-center items-center gap-[30px] w-[340px] h-fit px-5 py-[30px] bg-black-60 border border-black-70 rounded-xl tablet:w-full tablet:px-[30px] mobile:px-5',
+        'sticky top-[100px] flex flex-col justify-center items-center gap-[30px] w-[340px] h-fit px-5 py-[30px] bg-black-60 border border-black-70 rounded-xl tablet:w-full tablet:px-[30px] mobile:px-5',
         props.className || '',
       )}
     >
