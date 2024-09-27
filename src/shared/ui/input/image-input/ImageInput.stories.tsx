f5a54a7004701ec173e81b5ded45ea035c81bcee
JaeBeen95 <nin2348@gmail.com>
2024-09-27 21:43:45 +0900

    ✨ Feat: 이미지 input 스토리북 개선

--- src/shared/ui/input/image-input/ImageInput.stories.tsx
+++ src/shared/ui/input/image-input/ImageInput.stories.tsx
@@ -1,40 +1,52 @@
 import ImageInput from './ImageInput'
+import { Meta, StoryObj } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { defaultProduct } from '@shared/icons'
-import type { Meta, StoryObj } from '@storybook/react'
-
-type Story = StoryObj<typeof ImageInput>
 
 const meta: Meta<typeof ImageInput> = {
-  title: 'shared/ui/input/ImageInput',
+  title: 'UI/ImageInput',
   component: ImageInput,
   tags: ['autodocs'],
   argTypes: {
+    imageIndex: {
+      control: 'number',
+      description: '이미지의 인덱스를 나타냅니다.',
+    },
     onCancel: { action: 'cancelled' },
     onSuccess: { action: 'succeeded' },
-    onFailed: { action: 'failed' },
-    imageIndex: { control: 'number' },
-    previewImage: { control: 'text' },
-  },
-}
-
-export const Default: Story = {
-  args: {
-    onCancel: action('onCancel'),
-    onSuccess: async (file, index) => {
-      action('onSuccess')(file, index)
-      return Promise.resolve()
+    previewImage: {
+      control: 'text',
+      description: '미리보기 이미지의 URL입니다.',
     },
-    onFailed: action('onFailed'),
-    imageIndex: 0,
-  },
-}
-
-export const WithPreviewImage: Story = {
-  args: {
-    ...Default.args,
-    previewImage: defaultProduct,
   },
 }
 
 export default meta
+
+type Story = StoryObj<typeof ImageInput>
+
+const Template: Story = {
+  render: (args) => <ImageInput {...args} />,
+}
+
+export const ImageInputExample: Story = {
+  ...Template,
+  args: {
+    imageIndex: 1,
+    onCancel: action('onCancel'),
+    onSuccess: async (file: File, index: number) => {
+      action('onSuccess')(file, index)
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      return Promise.resolve()
+    },
+    className: 'w-[135px] h-[135px]',
+  },
+}
+
+export const WithPreviewImage: Story = {
+  ...Template,
+  args: {
+    ...ImageInputExample.args,
+    previewImage: defaultProduct,
+  },
+}
