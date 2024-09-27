ccbcc1f60d8ae655792e862cc86be1f2eab5dfa5
d5ng <dong5ffice@gmail.com>
2024-09-27 13:13:40 +0900

    💄 [#191] Design: Button States 추가

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -5,7 +5,7 @@ import type { ButtonProps } from './Button.type'
 
 export default function Button({ disabled, isLoading, ...props }: ButtonProps) {
   const commonStyle =
-    'relative font-semibold rounded-lg text-lg h-[65px] w-full disabled:cursor-not-allowed disabled:text-black-30 tablet:h-[55px] tablet:text-base mobile:h-[50px]'
+    'relative font-semibold rounded-lg text-lg h-[65px] w-full disabled:cursor-not-allowed disabled:text-black-30 tablet:h-[55px] tablet:text-base mobile:h-[50px] transition'
   const variant = BUTTON_VARIANT[props.variant]
   const defaultStyle = disabled ? variant.disabled : variant.default
 
