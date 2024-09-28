084052cf6e3ba939d32d3910cfcbb7795eb7764b
d5ng <dong5ffice@gmail.com>
2024-09-28 19:48:06 +0900

    성능테스트 - data를 클라이언트 측에서 로드

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,40 +14,46 @@ export default function Home() {
   )
 }
 
-// export const getServerSideProps = (async (context) => {
-//   const cookie = context.req.cookies.auth
+export const getServerSideProps = (async (context) => {
+  const cookie = context.req.cookies.auth
 
-//   if (cookie) {
-//     const token = JSON.parse(cookie).accessToken
-//     axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-//   } else {
-//     axiosInstance.defaults.headers.common['Authorization'] = ``
-//   }
+  if (cookie) {
+    const token = JSON.parse(cookie).accessToken
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  } else {
+    axiosInstance.defaults.headers.common['Authorization'] = ``
+  }
 
-//   try {
-//     const queryClient = new QueryClient()
+  return {
+    props: {
+      cookie: cookie ? cookie : null,
+    },
+  }
 
-//     await queryClient.prefetchQuery({
-//       queryKey: ['product', 'reviewCount'],
-//       queryFn: () => fetchProducts({ order: 'reviewCount' }),
-//     })
+  // try {
+  //   const queryClient = new QueryClient()
 
-//     await queryClient.prefetchQuery({
-//       queryKey: ['product', 'ratedCount'],
-//       queryFn: () => fetchProducts({ order: 'rating' }),
-//     })
+  //   await queryClient.prefetchQuery({
+  //     queryKey: ['product', 'reviewCount'],
+  //     queryFn: () => fetchProducts({ order: 'reviewCount' }),
+  //   })
 
-//     const hotData = queryClient.getQueryData(['product', 'reviewCount'])
-//     const ratedData = queryClient.getQueryData(['product', 'ratedCount'])
+  //   await queryClient.prefetchQuery({
+  //     queryKey: ['product', 'ratedCount'],
+  //     queryFn: () => fetchProducts({ order: 'rating' }),
+  //   })
 
-//     if (!hotData && !ratedData) return { notFound: true }
-//     return {
-//       props: {
-//         dehydratedState: dehydrate(queryClient),
-//         cookie: cookie ? cookie : null,
-//       },
-//     }
-//   } catch (error) {
-//     return { notFound: true }
-//   }
-// }) satisfies GetServerSideProps
+  //   const hotData = queryClient.getQueryData(['product', 'reviewCount'])
+  //   const ratedData = queryClient.getQueryData(['product', 'ratedCount'])
+
+  //   if (!hotData && !ratedData) return { notFound: true }
+  //   return {
+  //     props: {
+  //       dehydratedState: dehydrate(queryClient),
+  //       cookie: cookie ? cookie : null,
+  //     },
+  //   }
+  // } catch (error) {
+  //   return { notFound: true }
+  // }
+}) satisfies GetServerSideProps
