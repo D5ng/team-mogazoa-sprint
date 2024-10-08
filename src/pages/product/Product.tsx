406757a34495a523c174a781afed0707b75b9366
wooluck <155030236+parkwoohyeok@users.noreply.github.com>
2024-10-08 13:03:34 +0900

    fix : product page 카테고리 키 삭제

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -30,7 +30,7 @@ export default function ProductPage() {
         </div>
         <div className="w-[46vw] tablet:w-[68vw] ml-[24.5vw] tablet:ml-[220px] mobile:w-[89vw] mobile:ml-[30px] flex flex-col gap-[80px] tablet:gap-[60px] overflow-hidden">
           <Ranking />
-          {inputValue || selectedCategoryKey ? (
+          {inputValue ? (
             <Suspense fallback={<ProductCardSectionSkeleton count={6} />}>
               <ProductSearched />
             </Suspense>
