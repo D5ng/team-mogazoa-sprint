a788ba628c38acdd5f406c21f8e4bbe28994f567
d5ng <dong5ffice@gmail.com>
2024-10-18 13:25:44 +0900

    ✨ [#263] Feat: Oauth 실패 페이지 추가

--- src/pages/auth/oauth-sign-up/OauthSignUpPage.tsx
+++ src/pages/auth/oauth-sign-up/OauthSignUpPage.tsx
@@ -2,7 +2,7 @@ import { OauthSignUpField } from '@widgets/auth/components'
 
 export default function OauthSignUpPage() {
   return (
-    <main className="w-full min-h-screen flex justify-center items-center mt-24 tablet:mt-48 mobile:mt-20 bg-black-80">
+    <main className="w-screen h-screen flex flex-col items-center justify-center">
       <OauthSignUpField />
     </main>
   )
